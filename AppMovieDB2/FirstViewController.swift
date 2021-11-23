57aac5caf11f434b9e80ecabce6406640d5c413c
Jordan Dominguez <jordan.florindez@gmail.com>
2021-11-22 22:31:10 -0500

    Initial Commit

---
+++ AppMovieDB2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AppMovieDB2
+//
+//  Created by Mac 10 on 11/22/21.
+//  Copyright © 2021 empresa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppMovieDB2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AppMovieDB2
+//
+//  Created by Mac 10 on 11/22/21.
+//  Copyright © 2021 empresa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
